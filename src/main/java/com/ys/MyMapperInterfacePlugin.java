ff112320b64ded06208a23808c83a82e77dc9036
44072429 <44072429@qq.com>
2018-05-14 08:35:06 +0800

    部分bug修正

--- src/main/java/com/ys/MyMapperInterfacePlugin.java
+++ src/main/java/com/ys/MyMapperInterfacePlugin.java
@@ -1,7 +1,10 @@
 package com.ys;
 
+import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.Plugin;
 import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Field;
 import org.mybatis.generator.api.dom.java.Interface;
 import org.mybatis.generator.api.dom.java.TopLevelClass;
 
@@ -13,6 +16,45 @@ import java.util.List;
 public class MyMapperInterfacePlugin extends PluginAdapter {
 
     public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    /**
+     * Model文件被生成时候调用
+     * @param topLevelClass
+     * @param introspectedTable
+     * @return
+     */
+    @Override
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass,
+                                                 IntrospectedTable introspectedTable) {
+
+        topLevelClass.addJavaDocLine( "import io.swagger.annotations.ApiModel;" );
+        topLevelClass.addJavaDocLine( "import io.swagger.annotations.ApiModelProperty;" );
+
+        topLevelClass.addJavaDocLine( "" );
+        topLevelClass.addAnnotation( "@ApiModel" );
+
+        return true;
+    }
+
+    /**
+     * 数据库每个字段被生成的时候会调用
+     * @param field
+     * @param topLevelClass
+     * @param introspectedColumn
+     * @param introspectedTable
+     * @param modelClassType
+     * @return
+     */
+    @Override
+    public boolean modelFieldGenerated(Field field,
+                                       TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn,
+                                       IntrospectedTable introspectedTable,
+                                       Plugin.ModelClassType modelClassType) {
+
+        field.addAnnotation( "@ApiModelProperty(value=\"" + introspectedColumn.getRemarks() + "\")" );
+
         return true;
     }
 
